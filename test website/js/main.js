9f6afd8ce73566c31ce75c3c9be942f3842d68c5
yamenmaani <yamenmaanipy@gmail.com>
2023-04-17 07:56:46 +0300

    uploading the website

---
+++ test website/js/main.js
@@ -0,0 +1,222 @@
+let primary_color= "#131947";
+let dark_primary_color = "#0F1439";
+let special_color = "#F2B12E";
+let dark_special_color = "#CC9A34";
+function submitForm() {
+    const fileNumber = document.getElementById("fileNumber").value;
+    const patientName = document.getElementById("patientName").value;
+    const workType = document.getElementById("type-of-work").value;
+    const remarks = document.getElementById("remarks").value;
+    const test = document.getElementById("test").checked;
+    const color = document.getElementById("color-list").value;
+
+    const formDataDiv = document.getElementById("formData");
+    formDataDiv.innerHTML = `<p>File Number: ${fileNumber}</p>
+                            <p>Patient Name: ${patientName}</p>
+                            <p>Type of Work: ${workType}</p>
+                            <p>Remarks: ${remarks}</p>
+                            <p>Test: ${test}</p>
+                            <p>Color: ${color}</p>`;
+    console.log("File Number: " + fileNumber);
+    console.log("Patient Name: " + patientName);
+    console.log("Type of Work: " + workType);
+    console.log("Remarks: " + remarks);
+    console.log("Test: " + test);
+    console.log("Color: " + color);
+}
+function _11(evt) {
+    console.log('teeth 11 choosed');
+    document.getElementById("_11").classList.toggle("normal-num-color");
+    console.log("teeth 11 color changed");
+}
+
+function _12(evt) {
+    console.log('teeth 12 choosed');
+    document.getElementById("_12").classList.toggle("normal-num-color");
+    console.log("teeth 12 color changed");
+}
+
+function _13(evt) {
+    console.log('teeth 13 choosed');
+    document.getElementById("_13").classList.toggle("normal-num-color");
+    console.log("teeth 13 color changed");
+}
+
+function _14(evt) {
+    console.log('teeth 14 choosed');
+    document.getElementById("_14").classList.toggle("normal-num-color");
+    console.log("teeth 14 color changed");
+}
+
+function _15(evt) {
+    console.log('teeth 15 choosed');
+    document.getElementById("_15").classList.toggle("normal-num-color");
+    console.log("teeth 15 color changed");
+}
+
+function _16(evt) {
+    console.log('teeth 16 choosed');
+    document.getElementById("_16").classList.toggle("special-num-color");
+    console.log("teeth 16 color changed");
+}
+
+function _17(evt) {
+    console.log('teeth 17 choosed');
+    document.getElementById("_17").classList.toggle("special-num-color");
+    console.log("teeth 17 color changed");
+}
+
+function _18(evt) {
+    console.log('teeth 18 choosed');
+    document.getElementById("_18").classList.toggle("special-num-color");
+    console.log("teeth 18 color changed");
+}
+
+function _19(evt) {
+    console.log('teeth 19 choosed');
+    document.getElementById("_19").classList.toggle("normal-num-color");
+    console.log("teeth 19 color changed");
+}
+
+function _21(evt) {
+    console.log('teeth 21 choosed');
+    document.getElementById("_21").classList.toggle("normal-num-color");
+    console.log("teeth 21 color changed");
+}
+
+function _22(evt) {
+    console.log('teeth 22 choosed');
+    document.getElementById("_22").classList.toggle("normal-num-color");
+    console.log("teeth 22 color changed");
+}
+
+function _23(evt) {
+    console.log('teeth 23 choosed');
+    document.getElementById("_23").classList.toggle("normal-num-color");
+    console.log("teeth 23 color changed");
+}
+
+function _24(evt) {
+    console.log('teeth 24 choosed');
+    document.getElementById("_24").classList.toggle("normal-num-color");
+    console.log("teeth 24 color changed");
+}
+
+function _25(evt) {
+    console.log('teeth 25 choosed');
+    document.getElementById("_25").classList.toggle("normal-num-color");
+    console.log("teeth 25 color changed");
+}
+
+function _26(evt) {
+    console.log('teeth 26 choosed');
+    document.getElementById("_26").classList.toggle("special-num-color");
+    console.log("teeth 26 color changed");
+}
+
+function _27(evt) {
+    console.log('teeth 27 choosed');
+    document.getElementById("_27").classList.toggle("special-num-color");
+    console.log("teeth 27 color changed");
+}
+
+function _28(evt) {
+    console.log('teeth 28 choosed');
+    document.getElementById("_28").classList.toggle("special-num-color");
+    console.log("teeth 28 color changed");
+}
+function _31(evt) {
+    console.log('teeth 31 choosed');
+    document.getElementById("_31").classList.toggle("normal-num-color");
+    console.log("teeth 31 color changed");
+}
+
+function _32(evt) {
+    console.log('teeth 32 choosed');
+    document.getElementById("_32").classList.toggle("normal-num-color");
+    console.log("teeth 32 color changed");
+}
+
+function _33(evt) {
+    console.log('teeth 33 choosed');
+    document.getElementById("_33").classList.toggle("normal-num-color");
+    console.log("teeth 33 color changed");
+}
+
+function _34(evt) {
+    console.log('teeth 34 choosed');
+    document.getElementById("_34").classList.toggle("normal-num-color");
+    console.log("teeth 34 color changed");
+}
+
+function _35(evt) {
+    console.log('teeth 35 choosed');
+    document.getElementById("_35").classList.toggle("normal-num-color");
+    console.log("teeth 35 color changed");
+}
+
+function _36(evt) {
+    console.log('teeth 36 choosed');
+    document.getElementById("_36").classList.toggle("special-num-color");
+    console.log("teeth 36 color changed");
+}
+
+function _37(evt) {
+    console.log('teeth 37 choosed');
+    document.getElementById("_37").classList.toggle("special-num-color");
+    console.log("teeth 37 color changed");
+}
+
+function _38(evt) {
+    console.log('teeth 38 choosed');
+    document.getElementById("_38").classList.toggle("special-num-color");
+    console.log("teeth 38 color changed");
+}
+
+
+function _41(evt) {
+    console.log('teeth 41 choosed');
+    document.getElementById("_41").classList.toggle("normal-num-color");
+    console.log("teeth 41 color changed");
+}
+
+function _42(evt) {
+    console.log('teeth 42 choosed');
+    document.getElementById("_42").classList.toggle("normal-num-color");
+    console.log("teeth 42 color changed");
+}
+
+function _43(evt) {
+    console.log('teeth 43 choosed');
+    document.getElementById("_43").classList.toggle("normal-num-color");
+    console.log("teeth 43 color changed");
+}
+
+function _44(evt) {
+    console.log('teeth 44 choosed');
+    document.getElementById("_44").classList.toggle("normal-num-color");
+    console.log("teeth 44 color changed");
+}
+
+function _45(evt) {
+    console.log('teeth 45 choosed');
+    document.getElementById("_45").classList.toggle("normal-num-color");
+    console.log("teeth 45 color changed");
+}
+
+function _46(evt) {
+    console.log('teeth 46 choosed');
+    document.getElementById("_46").classList.toggle("special-num-color");
+    console.log("teeth 46 color changed");
+}
+
+function _47(evt) {
+    console.log('teeth 47 choosed');
+    document.getElementById("_47").classList.toggle("special-num-color");
+    console.log("teeth 47 color changed");
+}
+
+function _48(evt) {
+    console.log('teeth 48 choosed');
+    document.getElementById("_48").classList.toggle("special-num-color");
+}
